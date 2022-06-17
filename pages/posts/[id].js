feb9f84989c5a798f49d9f2edfb253831d351617
takamuu <mr.takamuu@gmail.com>
2022-06-17 10:29:27 +0900

    Update index.js and id.js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,7 @@
 import Layout from '../../components/layout';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
@@ -23,13 +26,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
